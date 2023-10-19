9123aa4c2e6fad3eaba3ac11ea53affd09c7219a
Vsevolond <vsevolond@gmail.com>
2023-10-19 21:09:24 +0300

    PROD: initial commit

---
+++ PVEditor/PVEditor/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PVEditor
+//
+//  Created by Всеволод Донченко on 19.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
